f8e8afebfaa672bf5f92cf338c3a24aba8217239
Emile Vrijdags <emile.vrijdags@gmail.com>
2016-04-20 11:23:27 +0200

    Moved debughelper class to its own file

---
+++ VoicepackCombiner/DebugTools/DebugHelpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using RecursionTracker.Plugins.VoicepackCombiner.GUI;
+
+namespace RecursionTracker.Plugins.VoicepackCombiner.DebugTools
+{
+    /// <summary>
+    /// Contains a debugForm and shows console window to help while debugging
+    /// </summary>
+    internal class DebugHelpers
+    {
+        private DebugForm _debugForm;
+
+        /// <summary>
+        /// Import win32 function to show console window
+        /// </summary>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        /// <summary>
+        /// Import win32 function to dispose console window
+        /// </summary>
+        [DllImport("kernel32.dll")]
+        private static extern int FreeConsole();
+
+        private VoicepackCombiner _voicepackCombiner;
+
+        internal DebugHelpers(VoicepackCombiner VoicepackCombiner)
+        {
+            _voicepackCombiner = VoicepackCombiner;
+            ShowCmdConsoleWindow();
+            SetDebugOutputToConsole();
+        }
+
+        private static void ShowCmdConsoleWindow()
+        {
+            AllocConsole();
+        }
+
+        /// <summary>
+        /// Calls to Debug.Write* will be printed in the console window
+        /// </summary>
+        private void SetDebugOutputToConsole()
+        {
+            StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            System.Diagnostics.Debug.Listeners.Add(new ConsoleTraceListener());
+        }
+
+        internal void ShowDebugForm()
+        {
+            if(_debugForm==null || !_debugForm.Visible)
+            { 
+                _debugForm = new DebugForm(_voicepackCombiner);
+                _debugForm.Show();
+            }
+        }
+
+        ~DebugHelpers()
+        {
+            FreeConsole();
+        }
+    }
+}
--- VoicepackCombiner/VoicepackCombinerPlugin.cs
+++ VoicepackCombiner/VoicepackCombinerPlugin.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using RecursionTracker.Plugins.VoicepackCombiner.DebugTools;
 using RecursionTracker.Plugins.VoicepackCombiner.GUI;
 
 #if DEBUG
-using System.IO;
-using System.Runtime.InteropServices;
+
 #endif
 
 namespace RecursionTracker.Plugins.VoicepackCombiner
@@ -64,65 +64,6 @@ namespace RecursionTracker.Plugins.VoicepackCombiner
     }
 
 #if DEBUG
-    /// <summary>
-    /// Contains a debugForm and shows console window to help while debugging
-    /// </summary>
-    internal class DebugHelpers
-    {
-        private DebugForm _debugForm;
-
-        /// <summary>
-        /// Import win32 function to show console window
-        /// </summary>
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool AllocConsole();
-
-        /// <summary>
-        /// Import win32 function to dispose console window
-        /// </summary>
-        [DllImport("kernel32.dll")]
-        private static extern int FreeConsole();
-
-        private VoicepackCombiner _voicepackCombiner;
-
-        internal DebugHelpers(VoicepackCombiner VoicepackCombiner)
-        {
-            _voicepackCombiner = VoicepackCombiner;
-            ShowCmdConsoleWindow();
-            SetDebugOutputToConsole();
-        }
-
-        private static void ShowCmdConsoleWindow()
-        {
-            AllocConsole();
-        }
-
-        /// <summary>
-        /// Calls to Debug.Write* will be printed in the console window
-        /// </summary>
-        private void SetDebugOutputToConsole()
-        {
-            StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-            Debug.Listeners.Add(new ConsoleTraceListener());
-        }
-
-        internal void ShowDebugForm()
-        {
-            if(_debugForm==null || !_debugForm.Visible)
-            { 
-                _debugForm = new DebugForm(_voicepackCombiner);
-                _debugForm.Show();
-            }
-        }
-
-        ~DebugHelpers()
-        {
-            FreeConsole();
-        }
-    }
 #endif
 
 }
